ba4ef7a9c250693c520cc73a96033cfd99d28cd2
nimit2801 <mnimitsavant@outlook.com>
2021-02-06 22:41:44 +0530

    dual working

--- src/bot.js
+++ src/bot.js
@@ -2,12 +2,13 @@
 require('dotenv').config();
 const colors = require('colors');
 const axios = require('axios');
-const { Client } = require('discord.js');
+const { Client, ReactionCollector } = require('discord.js');
 const spells = require('./commands/spells');
 
 // Commands
 const spell = require('./commands/spells');
 const prespective  = require('../api/perspective');
+const duel_spells = require('../DB/duel_spells.json');
 
 
 const client = new Client({
@@ -36,29 +37,31 @@ client.on('ready', () => {
     });
 })
 
-client.on('message', async (message) => {
-    if(message.author.bot === true) return;
-    const data_ = [];
-    prespective(message.content).then(data => {
-        // const p = data[0];
-        const t = data[0];
-        // const f = data[2];
-        // const s = data[3];
-         if(Number(t)>Number(0.89)){
-            console.log(t);
-            message.delete({time: 2000})
-                .then(message => {
-                    message.channel.send(`${message.id} from ${message.author} was deleted because it was against COC!`)
-                });
-         }
-            // console.log(p, t, f, s);
-    })
-    .catch(error => {
-        console.log(`ERROR: ${error}`);
-    });
-    // if(data[1]>Number(0.89))
-    //     message.delete();
-})
+// This thing is working DTTB
+// client.on('message', async (message) => {
+//     if(message.content.startsWith(PREFIX)) return;
+//     if(message.author.bot === true) return;
+//     const data_ = [];
+//     prespective(message.content).then(data => {
+//         // const p = data[0];
+//         const t = data[0];
+//         // const f = data[2];
+//         // const s = data[3];
+//          if(Number(t)>Number(0.89)){
+//             console.log(t);
+//             message.delete({time: 2000})
+//                 .then(message => {
+//                     message.channel.send(`${message.id} from ${message.author} was deleted because it was against COC!`)
+//                 });
+//          }
+//             // console.log(p, t, f, s);
+//     })
+//     .catch(error => {
+//         console.log(`ERROR: ${error}`);
+//     });
+//     // if(data[1]>Number(0.89))
+//     //     message.delete();
+// })
 
 client.on('message', async (message) => {
     if(message.author.bot === true) return;
@@ -96,33 +99,56 @@ client.on('message', async (message) => {
                     return;
                 }
                 
-                message.channel.send(`Do you except the challenge? ${dualists[1]} react with 👌 to accept`);
-                const filter = (reaction, user) => reaction.emoji.name === '👌' && (user.id === 'p');
-                message.awaitReactions(filter, { time: 15000 })
-                    .then(collected => {
-                        message.channel.send(`Lets play start dual ${message.author} vs ${args[0]}`);
-                        message.channel.send(`${message.author} you chance first.`)
-                        console.log(collected);
-                        console.log(`Collected ${collected.size} reactions`)
-                        
+                message.channel.send(`Do you accept the challenge? ${dualists[1]} react with 👌 to accept`)
+                .then( bot_message => {
+                    bot_message.react("👌");
+                    const filter = (reaction, user) => reaction.emoji.name === '👌' && user.id === p;
+                    let collector = bot_message.createReactionCollector(filter, { time: 10000 })
+                    collector.on('collect', (reaction, collector) => {
+                        console.log('got a reaction');
+                        let i =0;
+                        let user1 = {"health": 100, "id": dualists[0]};
+                        let user2 = {"health": 100, "id": dualists[1]};
+                        console.log(message.author.id, p);
+                        client.on('message', (message_fight) => {
+                            if(user1["health"] <= 0) return message_fight.channel.send(`${message.authour} lost the Dual!`);
+                            if(user2["health"] <= 0) return message_fight.channel.send(`${args[0]} lost the Dual!`);
+                            if(message_fight.author.id === message.author.id || message_fight.author.id === p){
+                                const [CMD_NAME, ...args_] = message_fight.content // = hp!spells sectumsempra @nimit
+                                    .trim().
+                                    substring(PREFIX.length)
+                                    .split(/\s+/);
+                                    if(i%2 === 0){
+                                        for(let i=0; i<Number(duel_spells.length); i++){
+                                            if(duel_spells[i].SpellName === args_[0]){
+                                                user2['health'] = Number(user2['health']) - Number(duel_spells[i].Damage);
+                                                console.log(`user2: ${user2['health']}`);
+                                            }
+                                        }
+                                    }else{
+                                        for(let i=0; i<Number(duel_spells.length); i++){
+                                            if(duel_spells[i].SpellName === args_[0]){
+                                                user1['health'] = Number(user1['health']) - Number(duel_spells[i].Damage);
+                                                console.log(`user1: ${user1['health']}`);
+                                            }
+                                        }
+                                    }
+                                
+                                console.log(args_, i);
+                                i = i+1;
+                            }
+                        })
                     })
-                    .catch(console.error);
-<<<<<<< HEAD
-                    //spell list - veer
-                    //individual spells - krish
-                    //mod spells - karandev
-                    //duel spells - veer
-                    
-=======
->>>>>>> bd5a59373cfbe51a2df0e242fe609240a9e16f17
-            } 
-            
+                    collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            }
             break;
         }
     }
 })
 
 
-
-
-client.login('ODA3MzgzNDg0OTY4NTk5NjE0.YB3Mdw.fCRfc51o1FCAsDkmMEXkADGP4Uc');
+client.login(process.env.DJSTOKEN); 
