a82c84b17c5de4e34b30e92bb2466e438a3fbf1a
nimit2801 <mnimitsavant@outlook.com>
2021-02-07 11:53:46 +0530

    updated src/bot

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,12 @@ const { Client, ReactionCollector } = require('discord.js');
 const spells = require('./commands/spells');
 const obliviate = require('./commands/obliviate');
 
+// calling json files
+const dualSpells = require('../DB/duel_spells.json');
+const senddata = require('./commands/dualSpellsEmbed');
+senddata(dualSpells);
+//console.log(dualSpells);
+
 // Commands
 const spell = require('./commands/spells');
 const prespective  = require('../api/perspective');
